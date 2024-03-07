9dd383e40b59ade878a9bb5e288748416e100778
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-07 16:04:35 -0500

    Add two components: Chat.js and Join.js . Import them to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 import {BroswerRouter as Router, Route} from 'react-router-dom'
 
+import Join from './components/Join'
+import Chat from './components/Chat'
+
+
 
 const App = ()=>(
 	<Router>
 		<Router path='/' exact component={Join}/>
+		<Router path='/chat' component={Chat}/>
 	</Router>
 );
+
+
+export default App;
