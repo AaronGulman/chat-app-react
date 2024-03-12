8c9ff719b04217273dde644b20fc963c7d1332d7
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-12 17:19:38 -0500

    Add socket.io connection handling

--- server/index.js
+++ server/index.js
@@ -4,9 +4,23 @@ const http = require('http');
 
 const PORT = process.env.PORT || 4000
 
+const router = require('./router');
+
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+
+io.on('connection', (socket) =>{
+console.log('we have a new conneciton')
+
+ socket.on('disconnect', ()=>{
+	console.log('user had left')
+ })
+})
+
+
+app.use(router)
 
 server.listen(PORT, ()=> console.log(`Server has started on port ${PORT}`));
