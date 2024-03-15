9d85c31724e1d43b171d707eade11873c962e917
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-14 22:50:51 -0500

    Update router to send JSON response"

--- server/router.js
+++ server/router.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/',(req,res) =>{
-	res.send('Server is running');
+router.get('/',(req, res) =>{
+	res.send({response: 'Server is running'}).status(200);
 });
 
 
