dc50d2e54d075fd09f68ad2d6cbd6c5c503c30d5
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-15 21:02:30 -0500

    Update Chat component to use useLocation hook and set up socket connection.

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,19 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client'
+import { useLocation } from 'react-router-dom';
 
-const Chat = ({ location }) => {
+
+let socket;
+
+const Chat = () => {
+	const location = useLocation();
+	const [name, setName] = useState('');
+	const [room, setRoom] = useState('');
+	const ENDPOINT = 'localhost:4000';
+	
 	useEffect(() =>{
-		const data = queryString.parse(location.search)
-		console.log(location.search)
-		console.log(data)
+		const {name , room} = queryString.parse(location.search);
 
-	})
+		socket = io(ENDPOINT)
+
+		setName(name);
+		setRoom(room);
+
+		return () => {
+			socket.disconnect();
+		      };
+		    }, [location.search]);
 
 
-	return(
-		<h1>Chat</h1>
-	)
-}
+return (
+	<div>
+	<h1>Chat</h1>
+	</div>
+);
+};
 
 export default Chat;
