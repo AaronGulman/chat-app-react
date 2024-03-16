3f8c5ddacfd09828026e98c70a1367bc9902dcb2
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-15 21:18:07 -0500

    Update import paths in App component to match new directory structure of Join and Chat components.

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 
-import Join from './components/Join'
-import Chat from './components/Chat'
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 
 
 const App = () => (
 	<Router>
 	  <Routes>
-	    <Route path='/' element={<Join />} />
-	    <Route path='/chat' element={<Chat />} />
+	    <Route path='/' element={<Join/>} />
+	    <Route path='/chat' element={<Chat/>} />
 	  </Routes>
 	</Router>
       );
