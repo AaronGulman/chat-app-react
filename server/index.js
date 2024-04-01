575f967482c1d211609ba2002762f91eb57abef2
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-31 23:01:10 -0500

    Rewrite the code for the server side index.js to Integrate Socket.io for real-time communication and router middleware

--- server/index.js
+++ server/index.js
@@ -1,15 +1,47 @@
 const express = require('express');
-const path = require('path');
-const app = express();
+const socketio = require('socket.io');
+const http = require('http');
 
-// Serve static files from the 'build' directory
+
+const PORT = process.env.PORT || 4000;
+
+const router = require('./router') ;
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const path = require('path');
+
+
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-// Define a route to serve 'index.html' for any route other than '/'
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-});
+io.on('connection', (socket)=>{
+    console.log('new connection')
+
+
+    socket.on('disconnect',()=>{
+        console.log('user left')
+    });
+})
+
+app.use(router)
+
+server.listen(PORT,() => console.log(`SERVER IS RUNNING ON PORT ${PORT}`))
+
+
+
+
+
+// const path = require('path');
+
+
+// app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+// });
 
 // Your existing server setup code...
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
